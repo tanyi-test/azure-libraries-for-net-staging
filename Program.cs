e2230ddc24fbc906f236e009c8787ce77a32da16
Tanyi Chen <tanchen@microsoft.com>
2020-01-03 16:32:20 +0800

    feat: add msi for net 5.0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using System;
+
+namespace azure_libraries_for_net_staging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usage = "Usage: dotnet run <subscription-id> <resource-group-name> [<client-id>]";
+            if (args.Length < 3)
+            {
+                throw new ArgumentException(usage);
+            }
+
+            var subscriptionId = args[0];
+            var resourceGroupName = args[1];
+            var clientId = args.Length > 2 ? args[2] : null;
+            var credentials = new AzureCredentials(new MSILoginInformation(MSIResourceType.VirtualMachine)
+            {
+                UserAssignedIdentityClientId = clientId
+            }, AzureEnvironment.AzureGlobalCloud);
+
+            var resourceManager = ResourceManager.Authenticate(credentials).WithSubscription(subscriptionId);
+            Console.WriteLine($"Got resource group: {resourceManager.ResourceGroups.GetByName(resourceGroupName).Id}");
+        }
+    }
+}
